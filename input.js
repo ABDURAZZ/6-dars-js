4ac9b1386791fe650f68a28d04a58b90dbfbdd69
ABDURAZZ <abdurazzokbossov@gmail.com>
2024-08-15 10:50:21 +0500

    Add files via upload

---
+++ input.js
@@ -0,0 +1,34 @@
+let son = +prompt('1-son kritin')
+let son2 = +prompt('2-son kritin')
+let amal = prompt('1) + 2 )- 3)* 4) / ')
+    console.log(son);
+    console.log(son2);
+    console.log(amal);
+
+    if (amal == 1) {
+     
+        alert(`${son} + ${son2} = ${son + son2}`)
+    }
+    else if (amal == 2) {
+     
+        alert(`${son} - ${son2} = ${son - son2}`)
+    }
+    else if (amal == 3) {
+     
+        alert(`${son} * ${son2} = ${son * son2}`)
+
+    }
+     else if (amal == 4) {
+     
+        alert(`${son} / ${son2} = ${son / son2}`)
+    }
+    
+    
+                   
+           
+
+
+
+
+
+
